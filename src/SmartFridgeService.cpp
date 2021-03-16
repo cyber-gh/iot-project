604de5985dd2f4b6a839f4223d4dde7db6909e1b
Valentin <>
2021-03-16 10:41:42 +0200

    add endpoint for get and set temperature

--- src/SmartFridgeService.cpp
+++ src/SmartFridgeService.cpp
@@ -10,6 +10,8 @@ void SmartFridgeService::setupRoutes() {
     using namespace Rest;
 
     Routes::Get(router, "/products", Routes::bind(&SmartFridgeService::getAllProducts, this));
+    Routes::Post(router, "/setTemperature/:temp", Routes::bind(&SmartFridgeService::setTemperature, this));
+    Routes::Get(router, "/getTemperature", Routes::bind(&SmartFridgeService::getTemperature, this));
 }
 
 void SmartFridgeService::getAllProducts(const Rest::Request &request, Http::ResponseWriter response) {
@@ -24,5 +26,28 @@ void SmartFridgeService::getAllProducts(const Rest::Request &request, Http::Resp
         }
 
         json j = ans;
+        response.send(Http::Code::Ok, j.dump());
+}
+
+
+void SmartFridgeService::setTemperature(const Rest::Request &request, Http::ResponseWriter response) { 
+        //curl -X POST localhost:9080/setTemperature/20
+        DatabaseAccess db = DatabaseAccess::getInstance();
+        auto temp = request.param(":temp").as<int>();
+        string query = Fridge::setTempQuery(temp);
+        db.executeQuery(query);
+        response.send(Http::Code::Ok);
+}
+
+
+
+void SmartFridgeService::getTemperature(const Rest::Request &request, Http::ResponseWriter response) {
+        //curl localhost:9080/getTemperature
+        DatabaseAccess db = DatabaseAccess::getInstance();
+        string query = Fridge::getTempQuery();
+        vector<vector<string>> v = db.selectQuery(query);
+        Fridge ans = Fridge::parse(v[0]);
+
+        json j = ans.temp;
         response.send(Http::Code::Ok, j.dump());
 }
