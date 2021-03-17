e8087ff0155275ac3d0cf945acf8bd42bf3965da
veeshanoo <visanu.cristian@gmail.com>
2021-03-17 16:07:05 +0200

    added example of post request with json body for adding a product

--- src/SmartFridgeService.cpp
+++ src/SmartFridgeService.cpp
@@ -10,9 +10,10 @@ void SmartFridgeService::setupRoutes() {
     using namespace Rest;
 
     Routes::Get(router, "/products", Routes::bind(&SmartFridgeService::getAllProducts, this));
+    Routes::Post(router, "/products", Routes::bind(&SmartFridgeService::insertProduct, this));
+
     Routes::Post(router, "/setTemperature/:temp", Routes::bind(&SmartFridgeService::setTemperature, this));
     Routes::Get(router, "/getTemperature", Routes::bind(&SmartFridgeService::getTemperature, this));
-    Routes::Post(router, "/products", Routes::bind(&SmartFridgeService::insertProduct, this));
 }
 
 void addJsonContentTypeHeader(Http::ResponseWriter &response) {
@@ -21,6 +22,8 @@ void addJsonContentTypeHeader(Http::ResponseWriter &response) {
 
 
 void SmartFridgeService::getAllProducts(const Rest::Request &request, Http::ResponseWriter response) {
+    addJsonContentTypeHeader(response);
+
     DatabaseAccess db = DatabaseAccess::getInstance();
 
 
@@ -32,18 +35,18 @@ void SmartFridgeService::getAllProducts(const Rest::Request &request, Http::Resp
     }
 
     json j = ans;
-    addJsonContentTypeHeader(response);
     response.send(Http::Code::Ok, j.dump());
 }
 
 
 void SmartFridgeService::setTemperature(const Rest::Request &request, Http::ResponseWriter response) { 
     //curl -X POST localhost:9080/setTemperature/20
+    addJsonContentTypeHeader(response);
+
     DatabaseAccess db = DatabaseAccess::getInstance();
     auto temp = request.param(":temp").as<int>();
     string query = Fridge::setTempQuery(temp);
     db.executeQuery(query);
-    addJsonContentTypeHeader(response);
     response.send(Http::Code::Ok);
 }
 
@@ -51,17 +54,33 @@ void SmartFridgeService::setTemperature(const Rest::Request &request, Http::Resp
 
 void SmartFridgeService::getTemperature(const Rest::Request &request, Http::ResponseWriter response) {
     //curl localhost:9080/getTemperature
+    addJsonContentTypeHeader(response);
+
     DatabaseAccess db = DatabaseAccess::getInstance();
     string query = Fridge::getTempQuery();
     vector<vector<string>> v = db.selectQuery(query);
     Fridge ans = Fridge::parse(v[0]);
 
     json j = ans.temp;
-    addJsonContentTypeHeader(response);
     response.send(Http::Code::Ok, j.dump());
 }
 
 void SmartFridgeService::insertProduct(const Rest::Request &request, Http::ResponseWriter response) {
-    cout << request.body() << '\n';
+    // curl -X POST -H "Content-Type: application/json" -d '{"date":"2021-03-23","id":1,"max_temp":20,"name":"test","quantity":3}' localhost:9080/products
+    addJsonContentTypeHeader(response);
+
+    try {
+        auto bodyJson = json::parse(request.body());
+        auto product = bodyJson.get<Product>();
+
+        DatabaseAccess db = DatabaseAccess::getInstance();
+        auto query = product.genInsertQuery();
+
+        db.executeQuery(query);
+    } catch (...) {
+        response.send(Http::Code::Bad_Request);
+        return;
+    }
+
     response.send(Http::Code::Ok);
 }
